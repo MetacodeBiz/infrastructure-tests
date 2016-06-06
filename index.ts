fa270c9976f6bfc35ac68af920468e951206291c
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2016-06-06 13:37:36 +0200

    Use node built-in assert module for better log messages

--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
 import { http2query, ipv6webserver, sslTest } from './infr';
 
-const assert = console.assert;
+import * as assert from 'assert';
 
 export async function testHttp20Positive() {
     const result = await http2query({ url: 'metacode.biz' });
-    assert(result.supports.http20 === true);
-    assert(result.supports.alpn === true, 'Must support ALPN');
+    assert.equal(result.supports.http20, true);
+    assert.equal(result.supports.alpn, true, 'Must support ALPN');
 }
 
 export async function testIpv6Positive() {
     const result = await ipv6webserver({ url: 'metacode.biz', scheme: 'https' });
-    assert(result.supports.ipv6 === true);
-    assert(result.info.dns.AAAA.length > 0);
-    assert(result.info.site.title.length > 0);
+    assert.equal(result.supports.ipv6, true);
+    assert.notEqual(result.info.dns.AAAA.length, 0);
+    assert.notEqual(result.info.site.title.length, 0);
 }
 
 export async function testSslPositive() {
     const result = await sslTest({ host: 'metacode.biz' });
-    assert(result.info.ssl.grades.length === 2);
-    assert(result.info.ssl.grades[0] === 'A+');
-    assert(result.info.ssl.grades[1] === 'A+');
+    assert.equal(result.info.ssl.grades.length, 2);
+    assert.equal(result.info.ssl.grades[0], 'A+');
+    assert.equal(result.info.ssl.grades[1], 'A+');
 }
--- infr_tests.ts
+++ infr_tests.ts
@@ -1,39 +1,39 @@
 import { http2query, ipv6webserver, sslTest } from './infr';
 
-const assert = console.assert;
+import * as assert from 'assert';
 
 export async function testHttp20Negative() {
     const result = await http2query({ url: 'example.com' });
-    assert(result.supports.http20 === false);
-    assert(result.supports.alpn === false);
+    assert.equal(result.supports.http20, false);
+    assert.equal(result.supports.alpn, false);
 }
 
 export async function testHttp20Positive() {
     const result = await http2query({ url: 'google.com' });
-    assert(result.supports.http20 === true);
-    assert(result.supports.alpn === true);
+    assert.equal(result.supports.http20, true);
+    assert.equal(result.supports.alpn, true);
 }
 
 export async function testIpv6Negative() {
     const result = await ipv6webserver({ url: 'zaxo.biz', scheme: 'http' });
-    assert(result.supports.ipv6 === false);
+    assert.equal(result.supports.ipv6, false);
 }
 
 export async function testIpv6Positive() {
     const result = await ipv6webserver({ url: 'example.com', scheme: 'http' });
-    assert(result.supports.ipv6 === true);
-    assert(result.info.dns.AAAA.length > 0);
-    assert(result.info.site.title === 'Example Domain');
+    assert.equal(result.supports.ipv6, true);
+    assert.notEqual(result.info.dns.AAAA.length, 0);
+    assert.equal(result.info.site.title, 'Example Domain');
 }
 
 export async function testSslNegative() {
     const result = await sslTest({ host: 'expired.badssl.com' });
-    assert(result.info.ssl.grades.length === 1);
-    assert(result.info.ssl.grades[0] === 'F');
+    assert.equal(result.info.ssl.grades.length, 1);
+    assert.equal(result.info.ssl.grades[0], 'F');
 }
 
 export async function testSslPositive() {
     const result = await sslTest({ host: 'sha256.badssl.com' });
-    assert(result.info.ssl.grades.length === 1);
-    assert(result.info.ssl.grades[0] === 'F');
+    assert.equal(result.info.ssl.grades.length, 1);
+    assert.equal(result.info.ssl.grades[0], 'F');
 }
