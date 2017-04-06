1f6d38174a4f1da925f4fc43c8ceda113128ab25
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2017-04-06 15:03:15 +0200

    Reduce code complexity by splitting tests into small functions

--- index.ts
+++ index.ts
@@ -3,20 +3,27 @@ import { sslTest, dnssec, headers } from './infr';
 import * as assert from 'assert';
 import isIPv6 = require('is-ipv6-node');
 
-export async function testSslPositive() {
-    const result = await sslTest({ host: 'metacode.biz' });
-    assert.equal(result.endpoints.length, 2);
+const endpoints = sslTest({ host: 'metacode.biz' }).then(result => result.endpoints);
 
-    // check for high grades
-    assert.equal(result.endpoints[0].grade, 'A+');
-    assert.equal(result.endpoints[1].grade, 'A+');
+export async function testHighGrades() {
+    assert.ok(
+        (await endpoints).every(endpoint => endpoint.grade === 'A+'),
+        'All endpoints must have grade A+.'
+    );
+}
 
-    // check for ALPN/H2 support
-    assert.ok(result.endpoints[0].details.supportsAlpn);
-    assert.ok(result.endpoints[1].details.supportsAlpn);
+export async function testAlpnSupport() {
+    assert.ok(
+        (await endpoints).every(endpoint => endpoint.details.supportsAlpn),
+        'All endpoints must support ALPN/H2.'
+    );
+}
 
-    // check for IPv6 support
-    assert.ok(result.endpoints.some(endpoint => isIPv6(endpoint.ipAddress)), 'Must have at least one IPv6 address.');
+export async function testIPv6Support() {
+    assert.ok(
+        (await endpoints).some(endpoint => isIPv6(endpoint.ipAddress)),
+        'Must have at least one IPv6 address.'
+    );
 }
 
 export async function testDnssecPositive() {
@@ -24,11 +31,10 @@ export async function testDnssecPositive() {
     assert.ok(result.secure);
 }
 
-export async function testHeadersPositive() {
-    const result = await headers({ url: 'https://metacode.biz' });
+const headersResult = headers({ url: 'https://metacode.biz' });
 
-    // Strict-Transport-Security
-    const sts = result.get('strict-transport-security');
+export async function testStrictTransportSecurity() {
+    const sts = (await headersResult).get('strict-transport-security');
     if (!sts) {
         throw new assert.AssertionError('STS header should be set.');
     } else {
@@ -36,9 +42,10 @@ export async function testHeadersPositive() {
         assert.ok('includeSubDomains' in sts, 'STS must include subdomains.');
         assert.ok(Number(sts['max-age']) >= 31536000, 'STS must be valid for long time.');
     }
+}
 
-    // X-XSS-Protection
-    const xss = result.get('x-xss-protection');
+export async function testXXSSProtection() {
+    const xss = (await headersResult).get('x-xss-protection');
     if (!xss) {
         throw new assert.AssertionError('XSS header should be set.');
     } else {
@@ -46,21 +53,22 @@ export async function testHeadersPositive() {
         assert.equal('block', xss['mode'], 'XSS must be in mode block.');
         assert.ok('report' in xss, 'XSS report url must be present.');
     }
+}
 
-    // X-Content-Type-Options
-    const cto = result.get('x-content-type-options');
+export async function testXContentTypeOptions() {
+    const cto = (await headersResult).get('x-content-type-options');
     if (!cto) {
         throw new assert.AssertionError('XCTO header should be set.');
     } else {
         assert.ok('nosniff' in cto, 'XCTO must be enabled.');
     }
+}
 
-    // X-Frame-Options
-    const xfo = result.get('x-frame-options');
+export async function testXFrameOptions() {
+    const xfo = (await headersResult).get('x-frame-options');
     if (!xfo) {
         throw new assert.AssertionError('XFO header should be set.');
     } else {
         assert.ok('DENY' in xfo, 'XFO must be enabled.');
     }
-
 }
